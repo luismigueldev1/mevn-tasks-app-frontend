9c89cf9797d5d1696f2b76dacf16c770043492d5
Luis Miguel <inf.lrodriguez@gmail.com>
2019-10-08 22:29:17 -0400

    Añadiendo Axios al proyecto

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 //Bootstrap
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+//Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+//Configuracion de bootstrap
+Vue.use(BootstrapVue)
+
+//Configuracion de Aaxios
+Vue.use(VueAxios, axios)
+axios.defaults.baseURL = 'http://localhost:3000/api';
 
 new Vue({
   router,
