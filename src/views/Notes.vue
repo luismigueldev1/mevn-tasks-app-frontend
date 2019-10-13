7fb517571370e6c55e650c447a773ec193ec30ea
Luis Miguel <inf.lrodriguez@gmail.com>
2019-10-12 22:17:51 -0400

    Consuminendo la API y mostrando los datos en nuestra views/Note

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,5 +1,56 @@
 <template>
-    <div>
-        <h1>Notas</h1>
-    </div>
+  <div class="container">
+    <h1>Notas</h1>
+    <p class="table" v-if="notes.length === 0">No hay notas para mostrar</p>
+    <p />
+    <table class="table" v-if="notes.length >= 1">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Descrición</th>
+          <th scope="col">Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(note, index) in notes" :key="index">
+          <td scope="row">{{ note._id }}</td>
+          <td>{{ note.nombre }}</td>
+          <td>{{ note.descripcion }}</td>
+          <td>MODIFY | DELETE</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      notes: []
+    };
+  },
+  created() {
+    this.getNotes();
+  },
+  methods: {
+    async getNotes() {
+      //Se puede usar fetch
+      /*  const BASE_URL = "http://localhost:3000/api/"
+        try{
+            const response = await fetch(BASE_URL + "notes")
+            const data = await response.json()
+            console.log(data)
+            this.notes = data
+        }catch(e){
+            console.log(e);
+            
+        } */
+      this.axios.get("/notes")
+        .then(res => (this.notes = res.data))
+        .catch(e => console.log(e.response));
+    }
+  }
+};
+</script>
