ca015d31251c792357e026e7592d0c0fdfc075a7
Luis Miguel <inf.lrodriguez@gmail.com>
2019-10-26 22:54:03 -0400

    Añadiendo funcionalidad de Agregar y Eliminar una nota

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="container">
-    <h1>Notas</h1>
+    <h1>Notes</h1>
+
     <b-alert
       :show="dismissCountDown"
       dismissible
       :variant="mensaje.color"
       @dismissed="dismissCountDown=0"
       @dismiss-count-down="countDownChanged"
-    >
-      {{mensaje.texto}}
-    </b-alert>
+    >{{mensaje.texto}}</b-alert>
+
+    <form @submit.prevent="addNote()">
+      <h3 class="form__title">Add new Note</h3>
+      <input
+        type="text"
+        class="form-control my-2"
+        placeholder="Nombre de la nota"
+        v-model="note.nombre"
+      />
+      <input
+        type="text"
+        class="form-control my-2"
+        placeholder="Descripción de la nota"
+        v-model="note.descripcion"
+      />
+      <b-button class="btn-success btn-block my-2" type="submit">Add new</b-button>
+    </form>
     <p class="table" v-if="notes.length === 0">No hay notas para mostrar</p>
     <p />
     <table class="table" v-if="notes.length >= 1">
@@ -23,27 +39,29 @@
       </thead>
       <tbody>
         <tr v-for="(note, index) in notes" :key="index">
-          <td scope="row">{{ note._id }}</td>
+          <td scope="row">{{ index+1 }}</td>
           <td>{{ note.nombre }}</td>
           <td>{{ note.descripcion }}</td>
-          <td> 
-            <b-button 
-            @click="alert()"
-            >
-            Acción
-            </b-button>
+          <td>
+            <b-button @click="updateNote(note._id)" class="btn-warning btn-sm ml-2">Update</b-button>
+            <b-button @click="deleteNote(note._id)" class="btn-danger btn-sm ml-2">Delete</b-button>
           </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-
+<style scoped>
+.form__title{
+  text-align: left;
+}
+</style>>
 <script>
 export default {
   data() {
     return {
       notes: [],
+      note: { nombre: "", descripcion: "" },
       mensaje: { color: "", texto: "" },
       dismissSecs: 5,
       dismissCountDown: 0
@@ -53,13 +71,6 @@ export default {
     this.getNotes();
   },
   methods: {
-    alert(){
-      this.mensaje = { 
-        color: "primary",
-        texto: "You shitty this bro." 
-       }
-       this.dismissCountDown = this.dismissSecs;
-    },
     async getNotes() {
       //Se puede usar fetch
       /*  const BASE_URL = "http://localhost:3000/api/"
@@ -76,6 +87,36 @@ export default {
         .get("/notes")
         .then(res => (this.notes = res.data))
         .catch(e => console.log(e.response));
+    },
+    addNote() {
+      this.axios
+        .post("/new", this.note)
+        .then(res => {
+          this.getNotes();
+          this.note = {};
+          this.mensaje = { color: "success", texto: "Note Added Successfuly" }
+          this.showAlert();
+        })
+        .catch(e => {
+          console.log(e.message);
+
+          this.mensaje = { color: "danger", texto: "Error Adding a New Note" }
+          this.showAlert();
+        });
+    },
+    deleteNote(id){
+      this.axios
+        .delete('/note/'+id)
+        .then(res =>{
+          this.getNotes()
+          this.mensaje = { color: "success", texto: "Note Deleted Successfuly" }
+          this.showAlert()
+        })
+        .catch(e =>{
+          this.mensaje = { color: "danger", texto: "Error Deleting a note" }
+          this.showAlert()
+        })
+
     },
     countDownChanged(dismissCountDown) {
       this.dismissCountDown = dismissCountDown;
